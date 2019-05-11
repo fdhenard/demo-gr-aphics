66be198052cc19b1ef3fdd30c96ca8726e35f75c
Frank Henard <fdhenard@gmail.com>
2019-05-11 11:53:32 -0500

    beginnings of post record route

--- src/demo_gr_aphics/web.clj
+++ src/demo_gr_aphics/web.clj
@@ -1,9 +1,13 @@
 (ns demo-gr-aphics.web
   (:require [mount.core :as mount]
             [ring.adapter.jetty :as jetty]
-            [reitit.ring :as ring]))
+            [reitit.ring :as ring]
+            [demo-gr-aphics.core :as core]))
 
 (def demog-recs (atom []))
+
+(defn post-demog-rec [request]
+  (swap! demog-recs conj (core/line->map "lname fname m red 2014-10-10" 1 (:space core/delimiter-regexes))))
 
 (def router
   (ring/router
@@ -12,7 +16,8 @@
                           :body "Hello World"})}]
     ["/ping" {:get (fn [req] {:status 200
                               :headers {"Content-Type" "text/html"}
-                              :body "Pong"})}]]))
+                              :body "Pong"})}]
+    ["/records" {:post post-demog-rec}]]))
 
 (def app (ring/ring-handler
           router
