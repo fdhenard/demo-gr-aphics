27127a8a58576194a6ce136e5a42398187c30cc9
Frank Henard <fdhenard@gmail.com>
2019-05-13 11:22:27 -0500

    refactor file processing for testability and readability

--- src/demo_gr_aphics/file.clj
+++ src/demo_gr_aphics/file.clj
@@ -6,11 +6,20 @@
 (def gender->display {:m "Male"
                       :f "Female"})
 
-(defn rec->displayable-for-cli [demog-rec]
+(defn rec->displayable-for-file-processing [demog-rec]
   (as-> demog-rec $
     (core/rec->displayable $)
     (assoc $ :gender (get gender->display (:gender $)))
     (clojure.walk/stringify-keys $)))
+
+(defn lines->canonical-maps [lines delimiter-regex]
+  (->> lines
+       (map-indexed
+        (fn [idx line]
+          (-> line
+              (core/line->map delimiter-regex)
+              (assoc :line-num (inc idx)))))
+       (group-by #(if (nil? (:result %)) :error :result))))
 
 (defn process-file! [filepath delimiter-name]
   (let [file (io/as-file filepath)]
@@ -19,34 +28,27 @@
       (let [file-as-str (slurp file)
             lines (clojure.string/split file-as-str #"\n")
             delimiter-regex (get core/delimiter-regexes (keyword delimiter-name))
-            xform-results (as-> lines $
-                            (map-indexed
-                             (fn [idx line]
-                               (-> line
-                                   (core/line->map delimiter-regex)
-                                   (assoc :line-num (inc idx))))
-                             $)
-                            (group-by #(if (nil? (:result %)) :error :result) $))
-            ;; _ (pprint/pprint xform-results)
-            errored-lines (->> (:error xform-results)
+            canonical-maps (lines->canonical-maps lines delimiter-regex)
+            ;; _ (pprint/pprint canonical-maps)
+            errored-lines (->> (:error canonical-maps)
                                (map #(-> %
                                          :error
                                          (assoc :line-num (:line-num %)))))
+            demog-recs (->> (:result canonical-maps)
+                            (map #(:result %)))
+            ;; _ (pprint/pprint demog-recs)
+            sorted-by-gender-lname (sort-by (juxt :gender :last-name) demog-recs)
+            display-sorted-gender-lname (map rec->displayable-for-file-processing sorted-by-gender-lname)
+            sorted-by-dob (sort-by :birthdate demog-recs)
+            display-sorted-dob (map rec->displayable-for-file-processing sorted-by-dob)
+            sorted-by-lname-desc (sort-by :last-name #(compare %2 %1) demog-recs)
+            display-sorted-lname-desc (map rec->displayable-for-file-processing sorted-by-lname-desc)
             ;; _ (pprint/pprint errored-lines)
             _ (println "\nerrors:\n")
             _ (doseq [errd-line errored-lines]
                 (println (str "error for line number " (:line-num errd-line)
                               ", line: \"" (:line errd-line) "\""))
                 (println (str (:spec-explain-str errd-line) "\n")))
-            demog-recs (->> (:result xform-results)
-                            (map #(:result %)))
-            ;; _ (pprint/pprint demog-recs)
-            sorted-by-gender-lname (sort-by (juxt :gender :last-name) demog-recs)
-            display-sorted-gender-lname (map rec->displayable-for-cli sorted-by-gender-lname)
-            sorted-by-dob (sort-by :birthdate demog-recs)
-            display-sorted-dob (map rec->displayable-for-cli sorted-by-dob)
-            sorted-by-lname-desc (sort-by :last-name #(compare %2 %1) demog-recs)
-            display-sorted-lname-desc (map rec->displayable-for-cli sorted-by-lname-desc)
             _ (println "\n\nOutput 1 – sorted by gender (females before males) then by last name ascending")
             _ (pprint/print-table display-sorted-gender-lname)
             _ (println "\n\nOutput 2 – sorted by birth date, ascending")
