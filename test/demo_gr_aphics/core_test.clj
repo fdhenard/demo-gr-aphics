95c6e1574bb51fae21e532e2d7c5a044536a8824
Frank Henard <fdhenard@gmail.com>
2021-04-02 13:37:47 -0500

    cleanup

--- test/demo_gr_aphics/core_test.clj
+++ test/demo_gr_aphics/core_test.clj
@@ -1,50 +1,48 @@
 (ns demo-gr-aphics.core-test
-  (:require [clojure.test :refer :all]
-            [demo-gr-aphics.core :refer :all]
-            [java-time :as time]))
+  (:require [clojure.test :as t]
+            [clojure.pprint :as pp]
+            [java-time :as time]
+            [demo-gr-aphics.core :as ut]))
 
 (defn get-problems-from-line-map [line-map]
   (get-in line-map [:spec-explain-data :clojure.spec.alpha/problems]))
 
-(deftest line->canonical-or-error-map-test
-  (testing "success"
-    (let [res (line->canonical-or-error-map "lname | fname | m | blue | 2013-03-04" pipe)
+(t/deftest line->canonical-or-error-map-test
+  (t/testing "success"
+    (let [res (ut/line->canonical-or-error-map "lname | fname | m | blue | 2013-03-04" ut/pipe)
           expecting {:type :demog-rec
                      :last-name "lname"
                      :first-name "fname"
                      :gender :m
                      :favorite-color "blue"
                      :birthdate (time/local-date "2013-03-04")}]
-      (is (= res expecting))))
-  (testing "general fail"
-    (let [actual (line->canonical-or-error-map "lname" pipe)]
-      (is (= :error (:type actual)))))
-  (testing "fail - gender invalid"
-    (let [actual (line->canonical-or-error-map "lname | fname | g | red | 2013-02-03" pipe)
-          ;; _ (clojure.pprint/pprint actual)
-          problems (get-problems-from-line-map actual)]
-      (is (and (not (nil? problems)) (= (count problems) 1)))
-      (let [problem (first problems)
-            ;; _ (clojure.pprint/pprint problem)
-            ]
-        (is (= (:path problem) [:gender])))))
-  (testing "fail - dob invalid"
-    (let [actual (line->canonical-or-error-map "lname | fname | f | red | wrong" pipe)
-          problems (get-problems-from-line-map actual)]
-      (is (and (not (nil? problems)) (= (count problems) 1)))
-      (let [problem (first problems)
-                ;; _ (clojure.pprint/pprint problem)
-                ;; _ (println (println (str "pred type: " (type (:pred problem)))))
-                ]
-            (is (= (:path problem) [:birthdate]))
-            (is (= (:pred problem) 'demo-gr-aphics.core/will-coerce-to-local-date?)))))
-  (testing "fail - dob invalid 2"
-    (let [actual (line->canonical-or-error-map "lname | fname | f | red | 2018-99-99" pipe)
-          problems (get-problems-from-line-map actual)]
-      (is (= (and (not (nil? problems)) (count problems)) 1))
-          (let [problem (first problems)
-                ;; _ (clojure.pprint/pprint problem)
-                ;; _ (println (println (str "pred type: " (type (:pred problem)))))
-                ]
-            (is (= (:path problem) [:birthdate]))
-            (is (= (:pred problem) 'demo-gr-aphics.core/will-coerce-to-local-date?))))))
+      (t/is (= res expecting))))
+  (t/testing "general fail"
+    (let [{:keys [type]} (ut/line->canonical-or-error-map "lname" ut/pipe)]
+      (t/is (= :error type))))
+  (t/testing "fail - gender invalid"
+    (let [actual (ut/line->canonical-or-error-map "lname | fname | g | red | 2013-02-03" ut/pipe)
+          #_ (pp/pprint actual)
+          problems (get-problems-from-line-map actual)
+          _ (t/is (and problems (= (count problems) 1)))
+          {:keys [path]} (first problems)
+            #_ (pp/pprint problem)]
+      (t/is (= path [:gender]))))
+  (t/testing "fail - dob invalid"
+    (let [actual (ut/line->canonical-or-error-map "lname | fname | f | red | wrong" ut/pipe)
+          problems (get-problems-from-line-map actual)
+          _ (t/is (and problems (= (count problems) 1)))
+          {:keys [path pred]} (first problems)
+          #_ (pp/pprint problem)
+          #_ (println (println (str "pred type: " (type (:pred problem)))))]
+      (t/is (= path [:birthdate]))
+      (t/is (= pred 'demo-gr-aphics.core/will-coerce-to-local-date?))))
+  (t/testing "fail - dob invalid 2"
+    (let [actual (ut/line->canonical-or-error-map "lname | fname | f | red | 2018-99-99" ut/pipe)
+          problems (get-problems-from-line-map actual)
+          _ (t/is (and problems (= (count problems) 1)))
+          #_ (pp/pprint problem)
+          #_ (println (println (str "pred type: " (type (:pred problem)))))
+          {:keys [path pred]} (first problems)]
+      (t/is (= path [:birthdate]))
+      (t/is (= pred 'demo-gr-aphics.core/will-coerce-to-local-date?)))))
--- test/demo_gr_aphics/file_test.clj
+++ test/demo_gr_aphics/file_test.clj
@@ -1,47 +1,45 @@
 (ns demo-gr-aphics.file-test
-  (:require [clojure.test :refer :all]
-            [demo-gr-aphics.file :refer :all]
+  (:require [clojure.test :as t]
+            [clojure.pprint :as pp]
+            [demo-gr-aphics.file :as ut]
             [demo-gr-aphics.core :as core]))
 
-(deftest lines->canonical-or-error-maps-test
-  (testing "a successful rec and an erroring rec"
+(t/deftest lines->canonical-or-error-maps-test
+  (t/testing "a successful rec and an erroring rec"
    (let [lines ["lname fname f indigo 1988-10-29"
                 "what"]
-         actual (lines->canonical-or-error-maps lines core/space)
-         ;; _ (clojure.pprint/pprint actual)
-         ]
-     (is (= 1 (count (:demog-rec actual))))
-     (is (= 1 (count (:error actual)))))))
+         {:keys [demog-rec error]} (ut/lines->canonical-or-error-maps lines core/space)
+         #_ (pp/pprint actual)]
+     (t/is (= 1 (count demog-rec)))
+     (t/is (= 1 (count error))))))
 
-(deftest canonical->displayable-for-file-processing-test
-  (testing "canonical to displayable for file processing transformation"
-   (let [demog-rec (core/line->canonical-or-error-map "lname fname f indigo 1988-10-29" core/space) 
-         actual (canonical->displayable-for-file-processing demog-rec)
-         ;; _ (clojure.pprint/pprint actual)
-         ]
-     (is (= actual {"last-name" "lname"
-                    "first-name" "fname"
-                    "gender" "Female"
-                    "favorite-color" "indigo"
-                    "birthdate" "10/29/1988"})))))
+(t/deftest canonical->displayable-for-file-processing-test
+  (t/testing "canonical to dt/isplayable for file processing transformation"
+    (let [demog-rec (core/line->canonical-or-error-map "lname fname f indigo 1988-10-29" core/space)
+          actual (ut/canonical->displayable-for-file-processing demog-rec)
+          #_ (pp/pprint actual)]
+      (t/is (= actual {"last-name" "lname"
+                       "first-name" "fname"
+                       "gender" "Female"
+                       "favorite-color" "indigo"
+                       "birthdate" "10/29/1988"})))))
 
-(deftest get-display-sorted-test
-  (testing "sorting for file output"
+(t/deftest get-display-sorted-test
+  (t/testing "sorting for file output"
    (let [lines ["lname1 fname1 m red 1990-11-08"
                 "lname2 fname2 f green 1980-01-13"]
          demog-recs (-> lines
-                        (lines->canonical-or-error-maps core/space)
+                        (ut/lines->canonical-or-error-maps core/space)
                         :demog-rec)
-         ;; _ (clojure.pprint/pprint demog-recs)
-         actual (get-display-sorted demog-recs #(sort-by :last-name sort-reverse %))
-         ;; _ (clojure.pprint/pprint actual)
-         ]
-     (is (= "lname2" (-> actual first (get "last-name"))))
-     (is (= "lname1" (-> actual last (get "last-name")))))))
+         #_ (pp/pprint demog-recs)
+         actual (ut/get-display-sorted demog-recs #(sort-by :last-name ut/sort-reverse %))
+         #_ (pp/pprint actual)]
+     (t/is (= "lname2" (-> actual first (get "last-name"))))
+     (t/is (= "lname1" (-> actual last (get "last-name")))))))
 
 
-;; process-file! is the remainder of the namespace that is not purely functional
+;; process-file! t/is the remainder of the namespace that t/is not purely functional
 ;; (side effecting).  The data transformations take place in the pure functions
-(deftest process-file-test
-  (testing "no errors in side effecting process-file! function"
-   (is (nil? (process-file! "resources/test-file.csv" "pipe")))))
+(t/deftest process-file-test
+  (t/testing "no errors in side effecting process-file! function"
+   (t/is (nil? (ut/process-file! "resources/test-file.csv" "pipe")))))
